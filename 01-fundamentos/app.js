dc38337023eb32adfa9f4362b434b8fdb9b79e06
Christian <christian.catamuscay.h@gmail.com>
2023-06-13 22:48:37 -0500

    S1-Introduccion: 12. Depuración y breakpoints

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -8,8 +8,13 @@ let a = 10,
 
 const saludo = c + d;
 
-// console.log('%c Mis variables','color:blue; font-weight: bold');
-// console.log({a});
-// console.log({b});
-// console.log({c});
-// console.table({a,b,c,d,x});
+console.log('%c Mis variables','color:blue; font-weight: bold');
+console.log({a});
+console.log({b});
+console.log({c});
+
+c = 'Hola de nuevo';
+
+console.table({a,b,c,d,x})
+
+
