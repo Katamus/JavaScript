f8ba0d7f38b98e69ec8b2727885a2d60cfc7fc29
Christian <christian.catamuscay.h@gmail.com>
2023-06-14 07:09:50 -0500

    S1-Introduccion: 15. Prompt, confirm y alert

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,10 @@
+alert('Hola Mundo');
+
+let nombre = prompt('Cual es tu nombre?');
+console.log(nombre);
+console.log('****' + nombre + '****');
+
+let rest = confirm('Esta seguro?');
+console.log(rest);
+
+// console.log(global);
