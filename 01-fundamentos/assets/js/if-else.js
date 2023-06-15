3888d09cea85c954c98d95e0e4a0e064a93b58c8
Christian <christiancatamuscay@r-fast.com>
2023-06-15 13:38:55 -0500

    S3-Ciclos y estructuras de control : 34. Laboratorio - Alternativa al if else

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -14,7 +14,7 @@ let dia = hoy.getDay();
 if( dia === 0 ){
     console.log('Domingo');
 } else if( dia === 1 ) {
-    console.log('Martes');
+    console.log('Lunes');
     // if(dia === 1){
     //     console.log('Lunes');
     // }else {
@@ -26,5 +26,18 @@ if( dia === 0 ){
     console.log('No es lunes, martes o domingo...');
 }
 
-
 console.log({dia});
+
+const diasLetrasArray = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+const diasLetras = {
+    0:'Domingo',
+    1:'Lunes',
+    2:'Martes',
+    3:'Miercoles',
+    4:'Jueves',
+    5:'Viernes',
+    6:'Sabado'
+}
+
+console.log(diasLetras[dia]||'Dia no definido');
+console.log(diasLetrasArray[dia]||'Dia no definido');
