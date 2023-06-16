818413c9282591d950c4511caf653de3108caabf
Christian <christiancatamuscay@r-fast.com>
2023-06-16 10:26:48 -0500

    S4-Laboratorio 1 - Blackjack : 55. Crear cartas en el HTML

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -7,6 +7,9 @@ let puntosComputadora = 0;
 // Referencias HTML
 const btnPedir = document.querySelector('#btnPedir');
 const puntosHtml = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputador = document.querySelector('#computadora-cartas');
+
 console.log(btnPedir);
 
 // Esta Funcion crea una nueva baraja
@@ -55,4 +58,20 @@ btnPedir.addEventListener('click', ()=>{
     const carta = pedirCarta();
     puntosJugador = puntosJugador + valorCarta(carta);
     puntosHtml[0].innerText = puntosJugador;
+
+    //<img src="assets/cartas/2C.png" class="carta"></img>
+    const imgCarta = document.createElement('img');
+    imgCarta.classList.add("carta");
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    divCartasJugador.append(imgCarta);
+    
+    if(puntosJugador > 21) {
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+    }else if(puntosJugador == 21 ) {
+        console.warn('21, genial!');
+        btnPedir.disabled = true;
+    }
+
+
 });
