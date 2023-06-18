03522ef636d2d2fa508a526343fff7723189c867
Christian <christiancatamuscay@r-fast.com>
2023-06-18 09:10:45 -0500

    S6-Clases en JavaScript y ESNext private properties : 75. Propiedades, gets y métodos estáticos

--- 01-fundamentos/assets/js/class/class.js
+++ 01-fundamentos/assets/js/class/class.js
@@ -1,4 +1,15 @@
 class Persona{
+
+    static _conteo = 0;
+
+    static get conteo(){
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje(){
+        console.log(this.nombre);
+        console.log('Hola a todos soy un metodo estatico');
+    }
     
     nombre = '';
     codigo = '';
@@ -15,6 +26,7 @@ class Persona{
        this.nombre = nombre;
        this.frase = frase;
 
+       Persona._conteo++;
     }
 
     set setComidaFavorita(comida){
@@ -38,17 +50,25 @@ class Persona{
 }
 
 const spiderman = new Persona('Peter parker','Spiderman', 'soy tu amigable vecino Spiderman');
-const ironman = new Persona('Tony Strak','Ironman','Yo soy Ironman');
+// const ironman = new Persona('Tony Strak','Ironman','Yo soy Ironman');
+
 spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
 
 
 // spiderman.nemesis = 'Duende verde'; // mala practica
 
 
-console.log(spiderman);
-console.log(ironman);
+// console.log(spiderman);
+// console.log(ironman);
 
 spiderman.miFase();
-ironman.miFase();
+// ironman.miFase();
 
-console.log(spiderman.getComidadFavorita);
+// console.log(spiderman.getComidadFavorita);
+// Persona._conteo = 2;
+console.log('Conteo stático',Persona._conteo);
+console.log(Persona.conteo);
+
+Persona.propiedadExterna = 'Hola Mundo';
+console.log(Persona);
+console.log( Persona.propiedadExterna );
