b6bfb706368f1ff65f33635f3f3a0db83dc71257
Christian <christiancatamuscay@r-fast.com>
2023-06-19 18:05:30 -0500

    S9- Laboratorio - Vite Lista de tareas: 118. Eliminar un Todo

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -44,6 +44,19 @@ export const App = (elementId) =>{
         displayTodos();
     });
 
+    todoListUL.addEventListener('click',(event)=>{
+
+        const isDestroyElement = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+        
+        if(!isDestroyElement || !element) return;
+
+        todoStore.deleteTodo(element.getAttribute('data-id'));
+        displayTodos();
+        
+       
+    });
+
 
     
 
