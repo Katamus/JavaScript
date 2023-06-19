98788205268f810bf5c481dbf21667c59df090fb
Christian <christiancatamuscay@r-fast.com>
2023-06-19 12:13:29 -0500

    S9- Laboratorio - Vite Lista de tareas: 111. Métodos que ocuparemos del Store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -20,6 +20,54 @@ const initStore = ()  => {
     console.log('InitSore palma');
 }
 
+const loadStore = () =>{
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filter.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
 export default {
-    initStore
+    initStore,
+    loadStore,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter
 }
