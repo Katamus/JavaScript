7eefac12e96ad02ae81831252a9d423cec7b6db3
Christian <christiancatamuscay@r-fast.com>
2023-06-22 07:48:52 -0500

    S10- Callbacks,promesas y generadores: 139. Manejo de errores en funciones asíncronas

--- 06-advanced/src/concepts/05-Async.js
+++ 06-advanced/src/concepts/05-Async.js
@@ -8,12 +8,12 @@ import { heroes } from "../data/heores";
 export const asyncComponent = (element) => {
     
     const id1 = '5d86371f97c29d020f1e1f6d';
-
+    console.log('Inicio de componente');
     findHero(id1)
-        .then((name) => element.innerHTML = name)
+        .then(console.log)
         .catch((error) => element.innerHTML = error);
     console.log(findHero(id1));
-
+    console.log('Fin de componente');
 }
 
 /**
@@ -23,5 +23,8 @@ export const asyncComponent = (element) => {
  */
 const findHero = async (id) => {
     const hero = heroes.find(hero => hero.id === id);
+    if(!hero){
+        throw new Error(`Hero with id ${id} not found`);
+    }
     return hero.name;
 }
