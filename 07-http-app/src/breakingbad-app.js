e42338134f7e63e0bb6493e7f05069867954ae74
Christian <christiancatamuscay@r-fast.com>
2023-06-22 11:22:32 -0500

    S11- Peticioes HTTP : 155. Tarea - Next Quote

--- 07-http-app/src/breakingbad-app.js
+++ 07-http-app/src/breakingbad-app.js
@@ -23,12 +23,17 @@ export const BreakingbadApp = async (element) => {
    const nextQuoteButton = document.createElement('button');
    nextQuoteButton.innerText = 'Next Quote';
 
-
    const renderQuote = (data) =>{
     quoteLabel.innerHTML = data.quote;
     authoLabel.innerHTML = data.author;
     element.replaceChildren(quoteLabel,authoLabel,nextQuoteButton);
    }
+
+
+   nextQuoteButton.addEventListener('click' , ()=>{
+        element.innerHTML = 'Loading....';
+        fetchQuote().then(renderQuote);
+    });
 
    fetchQuote().then(renderQuote);
 
