c0fcf565dd3966b94b34a10f573b400e49ca4a5d
Christian <christiancatamuscay@r-fast.com>
2023-06-22 08:03:55 -0500

    S10- Callbacks,promesas y generadores: 141. Errores en Async-Await

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -8,14 +8,16 @@ import { heroes } from "../data/heores";
 export const asynAwaitsComponent = async (element) => {
     
     const id1 = '5d86371f233c9f2425f16916';
-    const id2 = '5d86371f97c29d020f1e1f6d';
+    const id2 = '5d86371f97c29d020f1e1f6ds';
 
-
-    const hero1 = await findHero(id1);
-    const hero2 = await findHero(id2);
-
-
-    element.innerHTML = `${hero1.name} / ${hero2.name}`;
+    try {
+        const hero1 = await findHero(id1);
+        const hero2 = await findHero(id2);
+        element.innerHTML = `${hero1.name} / ${hero2.name}`;
+    } catch (error) {
+        console.log(error);
+        element.innerHTML = error
+    }
 
 
     console.log('asynAwaitComponent');
@@ -25,7 +27,7 @@ export const asynAwaitsComponent = async (element) => {
 const findHero = async (id) => {
     const hero  = heroes.find(hero => hero.id === id);
     if(!hero)
-        throw `Hero no found`;
+        throw new Error(`Hero whit ${id} no found`);
 
     return hero;
 }
