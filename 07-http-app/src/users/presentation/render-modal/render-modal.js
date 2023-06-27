a54f3737a22173cee49d42ec008bb1b43222aac2
Christian <christian.catamuscay.h@gmail.com>
2023-06-26 21:59:35 -0500

    S12- CRUD - App - No Frameworks : 171. Tomar la data del formulario

--- 07-http-app/src/users/presentation/render-modal/render-modal.js
+++ 07-http-app/src/users/presentation/render-modal/render-modal.js
@@ -12,7 +12,6 @@ export const renderModal = (element) => {
     modal = document.createElement('div');
     modal.innerHTML = modalHtml;
     modal.className = 'modal-container hide-modal';
-
     form = modal.querySelector('form');
 
     modal.addEventListener('click',(event)=>{
@@ -25,7 +24,22 @@ export const renderModal = (element) => {
 
     form.addEventListener('submit',(event) => {
         event.preventDefault();
-        console.log('Formulario enviado');
+        
+        const formData = new FormData(form);
+        const userLike = {};
+        for (const [key, value] of formData) {
+            if(key === 'balance'){
+                userLike[key] = +value;
+                continue;
+            }
+            if(key === 'isActive'){
+                userLike[key] =( value === 'on' ? true : false);
+                continue;
+            }
+            userLike[key] = value;
+        }
+        console.log(userLike);
+
     });
 
 }
@@ -36,4 +50,5 @@ export const showModal = () =>{
 
 export const hideModal = () =>{
     modal?.classList.add('hide-modal');
+    form?.reset();
 }
