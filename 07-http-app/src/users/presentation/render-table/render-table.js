47e57580af0adc08bfa76b831a96a29ea9ac4290
Christian <christian.catamuscay.h@gmail.com>
2023-06-26 23:04:00 -0500

    S12- CRUD - App - No Frameworks : 175. Cargar información del usuario al modal

--- 07-http-app/src/users/presentation/render-table/render-table.js
+++ 07-http-app/src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store';
+import { showModal } from '../render-modal/render-modal';
 import './render-table.css';
 
 let table;
@@ -21,7 +22,17 @@ const createTable = () => {
     return table;
 }
 
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListner = (event) => {
+    const element = event.target.closest('.select-user');
+    if(!element) return;
 
+    const id = element.getAttribute('data-id');
+    showModal(id);
+}
 
 /**
  * 
@@ -34,6 +45,7 @@ export const renderTable = (element) => {
     if(!table){
         table = createTable();
         element.append(table);
+        table.addEventListener('click',event=>tableSelectListner(event))
     }
 
     let tableHtml = '';
@@ -46,9 +58,9 @@ export const renderTable = (element) => {
             <td>${user.lastName}</td>
             <td>${user.isActive}  </td>
             <td>
-                <a href="#/" data-id="${user.id}">Select</a>
+                <a href="#/" class="select-user" data-id="${user.id}">Select</a>
                 |           
-                <a href="#/" data-id="${user.id}">Delete</a>                
+                <a href="#/" class="delete-user" data-id="${user.id}">Delete</a>                
             </td>
         </tr>`;
     });
